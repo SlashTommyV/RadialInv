3632911b294f9073a97217a168094ab9e2dc1566
SlashTommyV <tommyenjoyermail@gmail.com>
2025-07-06 14:09:53 +0200

    some stuff

--- src/StarterPack/Knife/KnifeScript.client.lua
+++ src/StarterPack/Knife/KnifeScript.client.lua
@@ -1,16 +1,16 @@
 --------------------- TEMPLATE BLADE WEAPON ---------------------------
 -- Waits for the child of the specified parent
 local function WaitForChild(parent, childName)
-	while not parent:FindFirstChild(childName) do parent.ChildAdded:wait() end
+	while not parent:FindFirstChild(childName) do
+		parent.ChildAdded:wait()
+	end
 	return parent[childName]
 end
-
 
 local SLASH_DAMAGE = 20
 local DOWNSTAB_DAMAGE = 35
 local THROWING_DAMAGE = 40
 local HOLD_TO_THROW_TIME = 0.38
-
 
 local Damage = 20
 
@@ -20,7 +20,7 @@ local MyCharacter = nil
 local MyPlayer = nil
 
 local Tool = script.Parent
-local Handle = WaitForChild(Tool, 'Handle')
+local Handle = WaitForChild(Tool, "Handle")
 
 local BlowConnection
 local Button1DownConnection
@@ -31,10 +31,10 @@ local PlayDownStab
 local PlayThrow
 local PlayThrowCharge
 
-local IconUrl = Tool.TextureId  -- URL to the weapon knife icon asset
+local IconUrl = Tool.TextureId -- URL to the weapon knife icon asset
 
-local DebrisService = game:GetService('Debris')
-local PlayersService = game:GetService('Players')
+local DebrisService = game:GetService("Debris")
+local PlayersService = game:GetService("Players")
 
 local SlashSound
 
@@ -51,60 +51,60 @@ function Blow(hit)
 end
 
 function BlowDamage(hit, damage)
-	local humanoid = hit.Parent:FindFirstChild('Humanoid')
+	local humanoid = hit.Parent:FindFirstChild("Humanoid")
 	local player = PlayersService:GetPlayerFromCharacter(hit.Parent)
 	if humanoid ~= nil and MyHumanoid ~= nil and humanoid ~= MyHumanoid then
-		if not MyPlayer.Neutral then 		
+		if not MyPlayer.Neutral then
 			-- Ignore teammates hit
 			if player and player ~= MyPlayer and player.TeamColor == MyPlayer.TeamColor then
 				return
 			end
 		end
 		-- final check, make sure weapon is in-hand
-		local rightArm = MyCharacter:FindFirstChild('Right Arm')
-		if (rightArm ~= nil) then
+		local rightArm = MyCharacter:FindFirstChild("Right Arm")
+		if rightArm ~= nil then
 			-- Check if the weld exists between the hand and the weapon
-			local joint = rightArm:FindFirstChild('RightGrip')
-			if (joint ~= nil and (joint.Part0 == Handle or joint.Part1 == Handle)) then
+			local joint = rightArm:FindFirstChild("RightGrip")
+			if joint ~= nil and (joint.Part0 == Handle or joint.Part1 == Handle) then
 				-- Make sure you only hit them once per swing
-					TagHumanoid(humanoid, MyPlayer)				
-					
-					local checksound = humanoid.Parent:findFirstChild("Sound")
-					if checksound == nil then
+				TagHumanoid(humanoid, MyPlayer)
+
+				local checksound = humanoid.Parent:findFirstChild("Sound")
+				if checksound == nil then
 					local givesound = script.Parent.Sound:clone()
 					givesound.Parent = humanoid.Parent
 					givesound.Disabled = false
-					else
+				else
 					checksound:remove()
 					local givesound = script.Parent.Sound:clone()
 					givesound.Parent = humanoid.Parent
 					givesound.Disabled = false
-					end
-					wait()
-					humanoid:TakeDamage(humanoid.MaxHealth)
-					if humanoid.Health == math.huge or humanoid.MaxHealth == math.huge then
-						humanoid.Parent:BreakJoints()
-					end
 				end
+				wait()
+				humanoid:TakeDamage(humanoid.MaxHealth)
+				if humanoid.Health == math.huge or humanoid.MaxHealth == math.huge then
+					humanoid.Parent:BreakJoints()
+				end
+			end
 		end
 	end
 end
 
 function TagHumanoid(humanoid, player)
 	-- Add more tags here to customize what tags are available.
-	while humanoid:FindFirstChild('creator') do
-		humanoid:FindFirstChild('creator'):Destroy()
-	end 
+	while humanoid:FindFirstChild("creator") do
+		humanoid:FindFirstChild("creator"):Destroy()
+	end
 
-	local creatorTag = Instance.new('ObjectValue')
+	local creatorTag = Instance.new("ObjectValue")
 	creatorTag.Value = player
-	creatorTag.Name = 'creator'
+	creatorTag.Name = "creator"
 	creatorTag.Parent = humanoid
 	DebrisService:AddItem(creatorTag, 1.5)
 
-	local weaponIconTag = Instance.new('StringValue')
+	local weaponIconTag = Instance.new("StringValue")
 	weaponIconTag.Value = IconUrl
-	weaponIconTag.Name = 'icon'
+	weaponIconTag.Name = "icon"
 	weaponIconTag.Parent = creatorTag
 	DebrisService:AddItem(weaponIconTag, 1.5)
 end
@@ -136,17 +136,22 @@ function ThrowAttack()
 	if PlayThrow then
 		PlayThrow.Value = true
 		wait()
-		if not Handle then return end
+		if not Handle then
+			return
+		end
 		local throwingHandle = Handle:Clone()
 		DebrisService:AddItem(throwingHandle, 5)
 		throwingHandle.Parent = game.Workspace
 		if MyCharacter and MyHumanoid then
 			throwingHandle.Velocity = (MyHumanoid.TargetPoint - throwingHandle.CFrame.p).unit * 100
 			-- set the orientation to the direction it is being thrown in
-			throwingHandle.CFrame = CFrame.new(throwingHandle.CFrame.p, throwingHandle.CFrame.p + throwingHandle.Velocity) * CFrame.Angles(0, 0, math.rad(-90))
-			local floatingForce = Instance.new('BodyForce', throwingHandle)
+			throwingHandle.CFrame = CFrame.new(
+				throwingHandle.CFrame.p,
+				throwingHandle.CFrame.p + throwingHandle.Velocity
+			) * CFrame.Angles(0, 0, math.rad(-90))
+			local floatingForce = Instance.new("BodyForce", throwingHandle)
 			floatingForce.force = Vector3.new(0, 196.2 * throwingHandle:GetMass() * 0.98, 0)
-			local spin = Instance.new('BodyAngularVelocity', throwingHandle)
+			local spin = Instance.new("BodyAngularVelocity", throwingHandle)
 			spin.angularvelocity = throwingHandle.CFrame:vectorToWorldSpace(Vector3.new(0, -400, 0))
 		end
 		Handle.Transparency = 1
@@ -156,7 +161,9 @@ function ThrowAttack()
 			local Throwevent = script.Parent.ThrowHandleHitWeld:Clone()
 			Throwevent.Parent = throwingHandle
 			Throwevent.Disabled = false
-			local touchedConn = throwingHandle.Touched:connect(function(hit) BlowDamage(hit, THROWING_DAMAGE) end)
+			local touchedConn = throwingHandle.Touched:connect(function(hit)
+				BlowDamage(hit, THROWING_DAMAGE)
+			end)
 		end
 		-- must check if it still exists since we waited
 		if throwingHandle then
@@ -172,21 +179,21 @@ function ThrowAttack()
 end
 
 function KnifeUp()
-	Tool.GripForward = Vector3.new(0,0,-1)
-	Tool.GripRight = Vector3.new(1,0,0)
-	Tool.GripUp = Vector3.new(0,1,0)
+	Tool.GripForward = Vector3.new(0, 0, -1)
+	Tool.GripRight = Vector3.new(1, 0, 0)
+	Tool.GripUp = Vector3.new(0, 1, 0)
 end
 
 function KnifeDown()
-	Tool.GripForward = Vector3.new(0,0,-1)
-	Tool.GripRight = Vector3.new(1,0,0)
-	Tool.GripUp = Vector3.new(0,-1,0)
+	Tool.GripForward = Vector3.new(0, 0, -1)
+	Tool.GripRight = Vector3.new(1, 0, 0)
+	Tool.GripUp = Vector3.new(0, -1, 0)
 end
 
 function KnifeOut()
-	Tool.GripForward = Vector3.new(0,0,-1)
-	Tool.GripRight = Vector3.new(1,0,0)
-	Tool.GripUp = Vector3.new(0,1,0)
+	Tool.GripForward = Vector3.new(0, 0, -1)
+	Tool.GripRight = Vector3.new(1, 0, 0)
+	Tool.GripUp = Vector3.new(0, 1, 0)
 end
 
 Tool.Enabled = true
@@ -199,7 +206,9 @@ function OnLeftButtonDown()
 end
 
 function OnLeftButtonUp()
-	if not Tool.Enabled then return end
+	if not Tool.Enabled then
+		return
+	end
 	-- Reset the list of hit players every time we start a new attack
 	HitPlayers = {}
 	if PlayThrowCharge then
@@ -208,8 +217,11 @@ function OnLeftButtonUp()
 	if Tool.Enabled and MyHumanoid and MyHumanoid.Health > 0 then
 		Tool.Enabled = false
 		local currTime = time()
-		if LeftButtonDownTime and currTime - LeftButtonDownTime > HOLD_TO_THROW_TIME and
-			currTime - LeftButtonDownTime < 1.15 then
+		if
+			LeftButtonDownTime
+			and currTime - LeftButtonDownTime > HOLD_TO_THROW_TIME
+			and currTime - LeftButtonDownTime < 1.15
+		then
 			ThrowAttack()
 		else
 			Attacking = true
@@ -225,17 +237,17 @@ function OnLeftButtonUp()
 end
 
 function OnEquipped(mouse)
-	PlayStabPunch = WaitForChild(Tool, 'PlayStabPunch')
-	PlayDownStab = WaitForChild(Tool, 'PlayDownStab')
-	PlayThrow = WaitForChild(Tool, 'PlayThrow')
-	PlayThrowCharge = WaitForChild(Tool, 'PlayThrowCharge')
- 	SlashSound = WaitForChild(Handle, 'Swoosh1')
-	HitSound = WaitForChild(Handle, 'Ting')
+	PlayStabPunch = WaitForChild(Tool, "PlayStabPunch")
+	PlayDownStab = WaitForChild(Tool, "PlayDownStab")
+	PlayThrow = WaitForChild(Tool, "PlayThrow")
+	PlayThrowCharge = WaitForChild(Tool, "PlayThrowCharge")
+	SlashSound = WaitForChild(Handle, "Swoosh1")
+	HitSound = WaitForChild(Handle, "Ting")
 	SlashSound:play()
 	BlowConnection = Handle.Touched:connect(Blow)
 	MyCharacter = Tool.Parent
-	MyTorso = MyCharacter:FindFirstChild('Torso')
-	MyHumanoid = MyCharacter:FindFirstChild('Humanoid')
+	MyTorso = MyCharacter:FindFirstChild("Torso")
+	MyHumanoid = MyCharacter:FindFirstChild("Humanoid")
 	MyPlayer = PlayersService.LocalPlayer
 	if mouse then
 		Button1DownConnection = mouse.Button1Down:connect(OnLeftButtonDown)
@@ -260,7 +272,6 @@ function OnUnequipped()
 	end
 	MyHumanoid = nil
 end
-
 
 Tool.Equipped:connect(OnEquipped)
 Tool.Unequipped:connect(OnUnequipped)
