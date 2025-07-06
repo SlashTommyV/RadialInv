d2009817dc3820fd709e42e932316b429f47555c
SlashTommyV <tommyenjoyermail@gmail.com>
2025-07-06 14:08:48 +0200

    some stuff

--- src/ReplicatedStorage/Modules/Shared/Maid.lua
+++ src/ReplicatedStorage/Modules/Shared/Maid.lua
@@ -1,9 +1,7 @@
 -- TOLIS MAID SYSTEM V1
 
-
 local Maid = {}
 Maid.__index = Maid
-
 
 function Maid.New()
 	local self = setmetatable({}, Maid)
@@ -14,7 +12,7 @@ end
 -- // Gives single connection to maid
 function Maid:GiveTask(task: any): ()
 	assert(task, "Given Task to maid is NIL")
-	
+
 	local taskId = #self._tasks + 1
 	self._tasks[taskId] = task
 end
@@ -23,7 +21,7 @@ end
 function Maid:GiveTaskID(key: string, task: any): ()
 	assert(task, "Given Task to maid is NIL")
 	assert(key, "Given Key to maid is NIL")
-	
+
 	self:CleanUp(key)
 	self._tasks[key] = task
 end
@@ -32,7 +30,7 @@ end
 function Maid:CleanUp(key: any): ()
 	if key then
 		local task = self._tasks[key]
-		
+
 		if task then
 			self._Clean(task)
 			self._tasks[key] = nil
@@ -48,19 +46,20 @@ end
 -- // Clean function for maid
 function Maid._Clean(task: any)
 	if typeof(task) == "RBXScriptConnection" then
-		if task.Connected then task:Disconnect() end
+		if task.Connected then
+			task:Disconnect()
+		end
 	elseif typeof(task) == "function" then
 		task()
-	elseif typeof(task) == "Instance" then task:Destroy() end
-	
+	elseif typeof(task) == "Instance" then
+		task:Destroy()
+	end
+
 	if typeof(task) == "thread" then
 		if coroutine.status(task) == "Suspended" then
 			coroutine.close(task)
 		end
 	end
 end
-
--- yo work pls and sync im so confused like fr
--- so why doesnt this work and  why cant i see it work in roblox studio this is so weird chat?
 
 return Maid
